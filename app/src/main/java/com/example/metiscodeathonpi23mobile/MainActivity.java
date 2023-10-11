e345ffbe940029a26294dfd660617442156d430b
BhavaniRaghavapuram <braghavapuram@asrcfederal.com>
2023-10-11 13:23:24 -0600

    Added Location and TrackedPath classes

--- app/src/main/java/com/example/metiscodeathonpi23mobile/MainActivity.java
+++ app/src/main/java/com/example/metiscodeathonpi23mobile/MainActivity.java
@@ -12,7 +12,11 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.sql.Time;
 import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.LocalTime; // import the LocalTime class
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private LocationManager locationManager;
@@ -24,8 +28,22 @@ public class MainActivity extends AppCompatActivity {
     //       we need a Location class for this with latitude, longitude, and sensor fields (e.g. elevation)
     //       locationList should be in a class called TrackedPath, which would also have the date/time stamp
     //       of when it was created, and we need to be able to serialize it to JSON to send to AWS
-    private ArrayList<String> locationList;
+    //private ArrayList<String> locationList;
 
+    class Location {
+        public double latitude;
+        public double longitude;
+        long elevation;
+    }
+
+    class TrackedPath {
+        ArratList<Location> locationList = new ArrayList<Location>();
+        LocalDate localDate = LocalDate.now(); // Create a date object
+        LocalTime localTime = LocalTime.now();
+    }
+
+    Location location = new Location();
+    TrackedPath trackedPath = new TrackedPath();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         btnStart = findViewById(R.id.btnStart);
         tvLocation = findViewById(R.id.tvLocation);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        locationList = new ArrayList<>();
+        //locationList = new ArrayList<>();
 
         btnStart.setOnClickListener(view -> handleButtonClick());
     }
@@ -60,12 +78,12 @@ public class MainActivity extends AppCompatActivity {
 
     private final LocationListener locationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
-            double longitude = location.getLongitude();
-            double latitude = location.getLatitude();
+            location.longitude = location.getLongitude();
+            location.latitude = location.getLatitude();
 
             // TODO: create a new Location object and add it to TrackedPath.locationList
-            String text = "(" + longitude + ", " + latitude + ")";
-            locationList.add(text);
+            String text = "(" + location.longitude + ", " + location.latitude + ")";
+            trackedPath.locationList.add(text);
             tvLocation.setText(text);
         }
     };
